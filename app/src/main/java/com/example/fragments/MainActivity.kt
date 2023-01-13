b0f7d419d69332eff9abfecb30b4effcb0f12fe9
shahb <malikshahbazzafar@gmail.com>
2023-01-13 23:59:25 +0500

    FirstCommit

--- app/src/main/java/com/example/fragments/MainActivity.kt
+++ app/src/main/java/com/example/fragments/MainActivity.kt
@@ -2,10 +2,35 @@ package com.example.fragments
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.commit
+import com.example.fragments.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private val firstFragment = FirstFragment()
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        if (savedInstanceState==null){
+            supportFragmentManager.commit {
+                setReorderingAllowed(true)
+                add(R.id.fragment_container_view,firstFragment)
+            }
+        }
+
+        binding.btnFirst.setOnClickListener {
+                supportFragmentManager.commit {
+                    show(firstFragment)
+                }
+        }
+
+        binding.btnSecond.setOnClickListener {
+            supportFragmentManager.commit {
+                hide(firstFragment)
+            }
+        }
+
     }
 }
